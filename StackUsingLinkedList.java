052627e55a3d26836a469234afbdd14d52cd7e4d
Gary Jiang <gary@host-studentw-139-21.dhcp.stevens-tech.edu>
2018-05-06 17:01:27 -0400

    Initial commmit to StackUsingLinkedList.java; The Class Stack implements with Linked List;

---
+++ StackUsingLinkedList.java
@@ -0,0 +1,53 @@
+/*********************************
+public class ListNode {
+	public int value;
+	public ListNode next;
+	public ListNode(int value) {
+		this.value = value;
+		next = null;
+	}
+}
+**********************************/
+
+// using a Linked List to implement a Stack
+// field: size;
+// function: boolean isEmpty(); void push(); Integer pop(); Integer top();
+public class Stack {
+	private int size;
+	private ListNode head;
+
+	private void push(int value) {
+		ListNode<Integer> node = new ListNode<Integer>(value);
+		if (head == null) {
+			head = node;
+		}
+		node.next = head;
+		head.next = node;
+		node = head;
+		size++;
+	}
+
+	private Integer pop() {
+		ListNode<Integer> node = new ListNode<>();
+		if (head == null) {
+			return null;
+		}
+
+		node = head;
+		head = head.next;
+		node.next = null;
+		return node.value;
+	}
+
+	private Integer top() {
+		if (head == null) {
+			return null;
+		}
+
+		return head.value;
+	}
+
+	private boolean isEmpty() {
+		return size == 0;
+	}
+}
