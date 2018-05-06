b2a1f8c8c1e335f0a8f71b0fd879da8f371d4587
Gary Jiang <gary@host-studentw-139-21.dhcp.stevens-tech.edu>
2018-05-06 17:13:13 -0400

    Initial commit without code

---
+++ PreOrderTraverseOfBinaryTree.java
@@ -0,0 +1,16 @@
+/*********************************
+public class TreeNode {
+	public int key;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode(int key) {
+		this.key = key;
+	}
+}
+**********************************/
+
+public class Solution {
+	public List<Integer> preOrder(TreeNode root) {
+		
+	}
+}
