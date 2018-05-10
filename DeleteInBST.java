15df4a35585c182d79c060ac06ad40cdee827775
Gary Jiang <gary.jiang@avon.com>
2018-05-10 10:41:34 -0400

    initial commmit DeleteInBST.java

---
+++ DeleteInBST.java
@@ -0,0 +1,16 @@
+/*******************************
+public TreeNode {
+	int value;
+	TreeNode left;
+	TreeNode right;
+
+	public TreeNode(int value) {
+		this.value = value;
+	}
+}
+*****************************/
+public class DeleteInBST {
+	public TreeNode deleteInBST(TreeNode root) {
+		
+	}
+}
