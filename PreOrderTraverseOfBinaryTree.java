5eb945b31b492f3ada8ab6d4760288aeefa388d7
Gary Jiang <gary@Garys-MBP.home>
2018-05-10 00:29:53 -0400

    first commit after the class

--- PreOrderTraverseOfBinaryTree.java
+++ PreOrderTraverseOfBinaryTree.java
@@ -11,6 +11,24 @@ public class TreeNode {
 
 public class Solution {
 	public List<Integer> preOrder(TreeNode root) {
-		
+		if (root == null) {
+			return root;
+		}
+		List<Integer> result = new ArrayList<>();
+		Deque<TreeNode> stack = new LinkedList<>();
+		stack.offerFirst(root);
+
+		while (!stack.isEmpty()) {
+			result.add(stack.pollFirst());
+
+			if (root.right != null) {
+				stack.offerFirst(root.right);
+			}
+			else if (root.left != null) {
+				stack.offerFist(root.left);
+				root = root.left;
+			}
+		}
+		return result;
 	}
 }
