27968dbdeda871324f8646f50230909126f8a120
Gary Jiang <cjiang0422@gmail.com>
2018-05-16 14:26:54 -0400

    first commit to PrintBinaryTree.java

---
+++ PrintBinaryTree.java
@@ -0,0 +1,47 @@
+/********************************
+class TreeNode {
+	int value;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int value) {
+		this.value = value;
+	}
+}
+********************************/
+
+public class PrintBinaryTree {
+
+	public void print(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+
+		Deque<TreeNode> queue = new LinkedList<>();
+		queue.offerFirst(root);// pollLast();
+
+		while (!queue.isEmpty()) {
+			// 因为要分层打印 所以首先找到每层的断点：当前层的queue为空；
+			int size = queue.size();
+
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.pollLast();
+				if (node.left != null) {
+					queue.offerFirst(node.left);
+				}
+				if (node.right != null) {
+					queue.offerFirst(node.right);
+				}
+
+				System.out.print(node.value + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+// step 1: expend a node;
+// step 2: generate its neighbor nodes;
+// step 3: terminate: queue is empty;
+// step 4: data structure: FIFO queue; 
