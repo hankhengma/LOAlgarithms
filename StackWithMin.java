c05efa9fba5db101532fa98c14be250f88f2e8d5
walrusCH <cjiang0422@gmail.com>
2018-05-24 19:58:32 -0400

    changed the code from myself to the standard code

--- StackWithMin.java
+++ StackWithMin.java
@@ -1,5 +1,3 @@
-StackWithMin.java
-
 /***************************************************************
 Enhance the stack implementation to support min() operation. min() should return the current minimum value in the stack. If the stack is empty, min() should return -1.
 
@@ -9,7 +7,7 @@ push(int element) - push the element to top
 top() - return the top element without remove it, if the stack is empty, return -1
 min() - return the current min value in the stack.
 **************************************************************/
-
+/***************************************************************
 public class Solution {
   private Stack<Integer> s1 = new Stack<Integer>(); // input
 
@@ -38,7 +36,6 @@ public class Solution {
   			return s2.pop();
   		}
   	}
-    
   }
   
   public void push(int element) {
@@ -53,7 +50,7 @@ public class Solution {
 	  		globalMin = element;
 	  		element = temp;
 	  	}
-	}
+	  }
   	s2.push(element);
   }
 
@@ -80,3 +77,45 @@ input: push(6),min(),push(5),push(9),top(),min(),pop(),min(),top().min(),pop(),m
 output: [6, 9, 5, 9, 5, 5, 5, 6, 6]
 expected:<[6, 9, 5, 9, 5, 5, 5, 5, 6]> 
  but was:<[6, 9, 5, 9, 5, 5, 5, 6, 6]>
+**************************************************************************/
+public class Solution {
+  private Deque<Integer> stack = new ArrayDeque<>();
+  private Deque<Integer> minStack = new ArrayDeque<>();
+
+  public Solution() {
+
+  }
+
+  public Integer pop () {
+    if (stack.isEmpty()) {
+      return null
+    }
+    int temp = stack.pollFirst();
+    if (temp == minStack.peekFirst()) {
+      minStack.pollFirst();
+    }
+    return temp;
+  }
+
+  public void push(int ele) {
+    if (minStack.isEmpty() || ele <= minStack.peekFirst()) {
+      minStack.offerFirst(ele);
+    }
+    stack.offerFirst(ele);
+  }
+
+  public Integer top() {
+    if (stack.isEmpty()) {
+      return null;
+    }
+    return stack.peekFirst();
+  }
+
+  public Integer min() {
+    if (minStack.isEmpty()) {
+      return null;
+    }
+    return minStack.pollFirst();
+  }
+
+}
