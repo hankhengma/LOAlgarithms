73f5280b3b7ce47056d77c05504985eee009b8be
walrusCH <cjiang0422@gmail.com>
2018-06-03 13:19:01 -0400

    initial commit to AllPermutationII with duplicate letters; Note: the set is used to filter the visited letters in curret level; Thus it should be initialized in the recursion.

--- AllPermutationII.java
+++ AllPermutationII.java
@@ -1,9 +1,39 @@
 // find all permutation of a string containing duplicate letters.
-public class Solution {
+public class AllPermutaionII {
 	public List<String> permutation(String input) {
 		List<String> result = new ArrayList<>();
 		if (input == null || inpurt.isEmpty()) {
 			return result;
 		}
+
+		char[] array = input.toCharArray();
+		int cur = 0;
+		
+		permutation(array, cur, visited, result);
+		return result;
+	}
+
+	private void permutation(char[] array, int cur, List<String> result) {
+		if (cur >= array.length - 1) {
+			result.add(Arrays.toString(array));
+			return;
+		}
+		// set is created inside the recursion
+		// because it is just recording
+		// distinct elements in current level
+		Set<Character> visited = new HashSet<>();
+		for (int i = cur; i < array.length; ++i) {
+			if (visited.add(array[cur])) {
+				swap(array, cur, i);
+				permutation(array, cur + 1, result);
+				swap(array, cur, i);
+			}
+		}
+	}
+
+	private void swap(char[] array, int left, int right) {
+		char temp = array[left];
+		array[left] = array[right];
+		array[right] = temp;
 	}
 }
