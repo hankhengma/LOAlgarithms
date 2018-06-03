3f94d5a96afab13e2bd550f9b6afd6392fabfb36
walrusCH <cjiang0422@gmail.com>
2018-06-03 14:21:01 -0400

    DP on LongestAscendingSubarray

---
+++ LongestAscedingSubarray.java
@@ -0,0 +1,26 @@
+public class LongestAscedingSubarray {
+	public int subarray(int[] array) {
+		if (array == null || array.length == 0) {
+			return array;
+		}
+		
+		int subarrayLength = 1;
+		int globalMax = 1;
+		// base cause: m[1] = 1;
+		// induction rule :
+		// m[i] = m[i - 1] + 1;
+		// if array[i] > array[i - 1] sublength + 1;
+		// otherwise sublength sets to 1;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[i - 1]) {
+				subarrayLength += 1;
+				if (subarrayLength >= globalMax) {
+					globalMax = subarrayLength;
+				}
+			} else {
+				subarrayLength = 1;
+			}
+		}
+		return globalMax;
+	}
+}
