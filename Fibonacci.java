cb71f171d40d74352bdd25fdf425abb0751353b6
walrusCH <cjiang0422@gmail.com>
2018-06-03 13:20:41 -0400

    Used two others ways, DP and Recursion + Memorization, than Recursion to reduce time complexity from O2^n to On

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,37 +1,53 @@
+import java.util.*;
+
 public class Solution {
-  public long fibonacci(int K) {
-    // Write your solution here
- 
-    
-    if (K < 0) {
-    	return 0;
-    }
-    /*******************************************
-    // the code will cause stackOverflowed.
-    else {
-    	if (K == 0) {
-    		return 0;
-      }
-      else if (K == 1) {
-        return 1;
-      }
-      else {
-        return fibonacci(K - 1) + fibonacci(K - 2);
-      }
-    }
-    ******************************************/
+    // Dynamic Programming
+    public long fibonacci(int K) {
+        // Write your solution here
+        if (K < 0) {
+            return 0;
+        }
+
 		else {
-      if (K <= 0) {
-      	return 0;
-      }
-      long[] array = new long[K + 1];
-      array[1] = 1;
-      
-      for (int i = 2; i <= K; i++) {
-      	array[i] = array[i - 2] + array[i - 1];
-      }
-      return array[K];
-      
+            if (K <= 0) {
+        	   return 0;
+            }
+            long[] array = new long[K + 1];
+            array[1] = 1;
+        
+            for (int i = 2; i <= K; i++) {
+        	   array[i] = array[i - 2] + array[i - 1];
+            }
+            return array[K];     
+        }
     }
-  }
+
+    // recursion + memorization
+    public long fibonacci(int K) {
+        if (K < 2) {
+            return K;
+        }
+        Map<Integer, Long> hashmap = new HashMap<>();
+        hashmap.put(0, (long)0);
+        hashmap.put(1, (long)1);
+        if (!hashmap.containsKey(K)) {
+            hashmap.put(K, fibonacci(K - 1) + fibonacci(K - 2));
+        }
+        return hashmap.get(K);
+    }
+
+    /*******************************************
+      // the code will cause stackOverflowed.
+      else {
+        if (K == 0) {
+            return 0;
+        }
+        else if (K == 1) {
+          return 1;
+        }
+        else {
+          return fibonacci(K - 1) + fibonacci(K - 2);
+        }
+      }
+      ******************************************/
 }
