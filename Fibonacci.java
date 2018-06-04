6b3dd479e2c59734173d873d7c51d9c7e1a143f3
walrusCH <cjiang0422@gmail.com>
2018-06-04 17:41:05 -0400

    Optimized the previous DP code;

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,22 +4,18 @@ public class Solution {
     // Dynamic Programming
     public long fibonacci(int K) {
         // Write your solution here
-        if (K < 0) {
+        if (K <= 0) {
             return 0;
         }
 
-		else {
-            if (K <= 0) {
-        	   return 0;
-            }
-            long[] array = new long[K + 1];
-            array[1] = 1;
-        
-            for (int i = 2; i <= K; i++) {
-        	   array[i] = array[i - 2] + array[i - 1];
-            }
-            return array[K];     
+        long[] array = new long[K + 1];
+        array[0] = 0;
+        array[1] = 1;
+    
+        for (int i = 2; i <= K; i++) {
+    	   array[i] = array[i - 2] + array[i - 1];
         }
+        return array[K];
     }
 
     // recursion + memorization
@@ -35,19 +31,4 @@ public class Solution {
         }
         return hashmap.get(K);
     }
-
-    /*******************************************
-      // the code will cause stackOverflowed.
-      else {
-        if (K == 0) {
-            return 0;
-        }
-        else if (K == 1) {
-          return 1;
-        }
-        else {
-          return fibonacci(K - 1) + fibonacci(K - 2);
-        }
-      }
-      ******************************************/
 }
