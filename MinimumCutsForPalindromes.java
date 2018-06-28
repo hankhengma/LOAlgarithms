faea5d8fd5f1a272921c2878f252937a6f4ecaaa
walrusCH <cjiang0422@gmail.com>
2018-06-27 21:48:57 -0400

    completed the entire code; and debug for the ArrayIndexOutOfBound

--- MinimumCutsForPalindromes.java
+++ MinimumCutsForPalindromes.java
@@ -6,6 +6,25 @@ Determine the fewest cuts needed for a palindrome partitioning of a given string
 
 public class MinimumCutsForPalindromes {
 	public int minCut(String input) {
-		
+		if (input == null || input < 2) {
+			return 0;
+		}
+		char[] inputArray = input.toCharArray();
+		int length = input.length();
+		int[] minCut = new int[length + 1];
+		boolean isPalindrome = new boolean[length + 1][length + 1];
+
+		for (int end = 1; end <= length; end++) {
+			minCut[end] = end;
+			for (int start = end; start >= 1; start--) {
+				if (inputArray[start - 1] == inputArray[end - 1]) {
+					isPalindrome[start][end] = end - start < 2 || isPalindrome[start + 1][end - 1];
+				}
+				if (isPalindrome[start][end]) {
+					minCut[end] = Math.min(minCut[end], minCut[start - 1] + 1);
+				}
+			}
+		}
+		return minCut[length] - 1;
 	}
 }
