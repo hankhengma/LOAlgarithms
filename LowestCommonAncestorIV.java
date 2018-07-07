ade5ed5e962744860c38587b37a931a3e0fba668
Chenghan Jiang <cjiang0422@gmail.com>
2018-07-07 13:15:21 -0400

    I Iterative algorithm: find out the first two nodes -- lca(node1, node2); use the lca(node1, node2) to find the lca with node3 -- lca(lca(node1, node2), node3) -- lca(node1, node3); ... lca(node1, nodek-1) with nodek -- lca(lca(node1, nodek-1),nodek) -- lca(node1, nodek);

---
+++ LowestCommonAncestorIV.java
@@ -0,0 +1,49 @@
+/******************
+find the LCA of k nodes in binary tree
+******************/
+public class LowestCommonAncestorIV {
+	static class TreeNode {
+		int key;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int key) {
+			this.key = key;
+		}
+	}
+	// find the LCA of two nodes first
+	// use the result to find the LCA with the rest nodes one by one
+	public TreeNode lca(TreeNode root, List<TreeNode> nodes) {
+		if (root == null || nodes == null || nodes.size() == 0) {
+			return null;
+		}
+		// initialization;
+		TreeNode[] result = new TreeNode[]{null};
+        TreeNode[] nodeArray = new TreeNode[nodes.size()];
+        int index = 0;
+        for (TreeNode node : nodes) {
+            nodeArray[index++] = node;
+        }
+        result[0] = findLca(root,nodeArray[0], nodeArray[1],result);
+
+        for (int i = 2; i < nodes.size(); i++) {
+            result[0] = findLca(root,nodeArray[i],result[0],result);
+        }
+        return result[0];
+	}
+
+	private TreeNode findLca(TreeNode root, TreeNode n1, TreeNode n2, TreeNode[] result) {
+		if (root == null || n1 == null || n2 == null) {
+			return root;
+		}
+
+		TreeNode left = findLca(root.left, n1, n2, result);
+		TreeNode right = findLca(root.right, n1, n2, result);
+
+		if (left != null && right != null) {
+			result[0] = root;
+		} else {
+			result[0] = left == null ? right : left;
+		}
+		return result[0];
+	}
+}
