8b52c2a0f1676fec1f7e4fac55a8c6b906f7943b
Chenghan Jiang <cjiang0422@gmail.com>
2018-07-14 20:02:23 -0400

    update: find the bug and debug; the induction rule has not been implemented correctly.

--- LongestAscendingSubsequence.java
+++ LongestAscendingSubsequence.java
@@ -8,11 +8,16 @@ public class LongestAscendingSubsequence {
 		int globalMax = 0;
 		m[0] = 1;
 		for (int i = 1; i < array.length; i++) {
+			// if m[i] < all the previous elements. then it should be 1 as it is a new start;
+			m[i] = 1;
 			for (int j = i - 1; j >= 0; j--) {
-				if (array[i] < array[j]) {
-					m[i] = 1;
-				} else {
-					m[i] = m[j] + 1;
+				// linear scan
+				// i = 2, j = 1;
+				// 3 > 2  -- > m[2] = m[1] + 1;
+				// i = 2, j = 0;
+				// 3 < 7 --> do nothing;
+				if (array[i] > array[j]) {
+					m[i] = Math.max(m[j] + 1, m[i]);
 				}
 			}
 			globalMax = Math.max(m[i], globalMax);
