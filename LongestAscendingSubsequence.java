b81fd48b0ec10e2fb1dc9a915f89780aff232df2
Chenghan Jiang <cjiang0422@gmail.com>
2018-07-15 15:02:32 -0400

    update: updated binary search method: O(nlogn); maintain a refined array to record the smallest end of every length; it is actually find the largest smaller element in the refined array;  eg: i 0 1 2 3 4 5 6 7 a 7 2 3 1 5 8 9 6 r 0 7 0 0 0 0 0 0 i = 1 array[i] = 2 r 0 2 0 0 0 0 0 0 i = 2; array[i= 3; r 0 2 3 0 0 0 0 0
    i = 3; array[i]=1; longest = 2;
    since 1 is the smallest element so far so the lenght should 1;
    r 0 1 3 0 0 0 0 0
    
    i = 4; array[i] = 5; longest = 2;  3 is the largest smaller element of 5;
    r 0 1 3 5 0 0 0 0
    
    i = 5, array[i]=8; longest=3; 5 is the largest smaller;
    r 0 1 3 5 8 0 0 0
    
    i = 6 array[i]=9; longest=4; 8 is the largest smaller;
    r 0 1 3 5 8 9 0 0
    
    i = 7 array[i]=6, longest=5; 5 is the largest smaller;
    r 0 1 3 5 6 9 0 0

--- LongestAscendingSubsequence.java
+++ LongestAscendingSubsequence.java
@@ -4,7 +4,7 @@ public class LongestAscendingSubsequence {
 			return 0;
 		}
 
-		int[] m = new int[array.length + 1];
+		int[] m = new int[array.length];
 		int globalMax = 0;
 		m[0] = 1;
 		for (int i = 1; i < array.length; i++) {
@@ -22,7 +22,42 @@ public class LongestAscendingSubsequence {
 			}
 			globalMax = Math.max(m[i], globalMax);
 		}
+		return globalMax;
 	}
+
+	public int LongestAscendingSubsequenceI(int[] array) {
+		if (array == null || array.length == 0) {
+			return 0;
+		}
+
+		int[] refined = new int[array.length + 1];
+		refined[1] = array[0];
+		int globalMax = 1;
+
+		for (int i = 1; i < array.length; i++) {
+			int index = find(refined, 1, globalMax, array[i]);
+
+			if (index == result) {
+				refined[++result] = array[i];
+			} else {
+				refined[index + 1] = array[i];
+			}
+		}
+		return globalMax;
+	}
+
+	private int find(int[] refined, int left, int right, int target) {
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (refined[mid] >= target) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return right;
+	}
+
 }
 
 /*********************
@@ -33,3 +68,9 @@ m 1 1 2 1 3 4 5 4
 
 m[0] = 1;
 m[i] 大几个数 就是个数+1
+
+mathod II: binary search
+i 0 1 2 3 4 5 6 7
+a 7 2 3 1 5 8 9 6 
+m 1 1 2 1 3 4 5 4
+r   1 3 5 6 9 
