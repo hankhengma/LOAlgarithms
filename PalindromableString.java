b9d1f738b1e97170b63d6d19ee45ab69a25fda9f
walrusCH <cjiang0422@gmail.com>
2018-09-06 00:01:01 -0400

    check if the given string can be transformed to a palindrome

--- PalindromableString.java
+++ PalindromableString.java
@@ -1,2 +1,11 @@
 //PalindromableString.java
 
+public class PalindromableString {
+	public boolean palindromable(String s) {
+		if (s == null || s.length() == 0) {
+			return true;
+		}
+
+		
+	}
+}
