3f4a3bbf23e3b503a1982d05b2326516c3e446c6
walrusCH <cjiang0422@gmail.com>
2018-09-06 00:19:20 -0400

    using count map array to store every char' appearence counts; if more than 1 letter have odd appearence - false otherwise true;

--- PalindromableString.java
+++ PalindromableString.java
@@ -6,6 +6,24 @@ public class PalindromableString {
 			return true;
 		}
 
-		
+		int[] countMap = new int[26];
+		char[] sArr = s.toCharArray();
+
+		for (int i = 0; i < sArr.length; ++i) {
+			countMap[sArr[i] - 'a']++;
+		}
+
+		int odd = 0;
+
+		for (int i = 0; i < 26; i++) {
+			if (countMap[i] % 2 == 1) {
+				++odd;
+			}
+		}
+		if (odd > 1) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
