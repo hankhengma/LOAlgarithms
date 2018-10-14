8c71bc5c46889148ec704e5c4bc8c7f349a6f9f0
Gary Jiang <walrusCH@users.noreply.github.com>
2018-10-14 17:20:44 -0400

    fix bugs which could be caused by the sequence of intervals

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -1,27 +1,26 @@
 import java.util.*;
 
 public class MergeIntervals {
-	static class Interval {
+	static class Interval implements Comparable<Interval> {
 		int start;
 		int end;
 		Interval() {start = 0; end = 0;}
 		Interval(int s, int e) { start = s; end = e;}
-	}
-
-	static class newComparator implements Comparator<Interval> {
+		
 		@Override
-		public int compare(Interval i1, Interval i2) {
-			return i1.start < i2.start ? -1 : i1.start == i2.start ? 0 : 1;
+		public int compareTo(Interval interval) {
+			return this.start < interval.start ? -1 : this.start == interval.start ? 0 : 1;
 		}
 	}
 
+	
 	public List<Interval> merge(List<Interval> intervals) {
 		if (intervals == null || intervals.size() == 0) {
 			return intervals;
 		}
 
 		LinkedList<Interval> merged = new LinkedList<>();
-
+		Collections.sort(intervals);
 		for (Interval interval : intervals) {
 			if (merged.isEmpty() || merged.getLast().end < interval.start) {
 				merged.add(interval);
